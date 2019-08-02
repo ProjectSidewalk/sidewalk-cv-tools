7f1b087e2fd7540e34620e8a2cd349ecff01f2a4
KaviMD <kaviasher@gmail.com>
2019-08-02 10:52:41 -0700

    Refactored folder structure and deleted unnecessary files

--- tools/samples/.ipynb_checkpoints/low_res_sample-checkpoint.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append("../")
-
-from pred_pano_labels import pred_pano_labels
-
-print pred_pano_labels("1a1UlhadSS_3dNtc5oI10Q", "../panos/", 13312, 6656, "../models/", num_threads=4, save_labeled_pano=True, verbose=True)
