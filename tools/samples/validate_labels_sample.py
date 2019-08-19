0987c74fc19f320e651a7b4a4983b7af77e06fd2
Devesh Sarda <deveshdgr8@gmail.com>
2019-08-19 15:33:16 -0700

    Removed tools folder

--- tools/samples/validate_labels_sample.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append("../")
-from cv_tools import generate_validation_data
-
-if __name__ == "__main__":
-    print(generate_validation_data("labels-testing.csv","../panos/", "sample"))
