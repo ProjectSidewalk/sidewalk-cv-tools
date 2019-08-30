d4a0817752de72ff3d5c88c5a1fa02494853c34d
Devesh Sarda <deveshdgr8@gmail.com>
2019-08-30 11:50:04 -0700

    Updated README and requirements file
    Changes them to reflect stuff needed for validaton and removed stuff for labeling

--- tools/samples/validate_labels_sample.py
+++
@@ -1,8 +0,0 @@
-# Written by Kavi Dey August 2019
-
-import sys
-sys.path.append("../")
-from cv_tools import generate_validation_data
-
-if __name__ == "__main__":
-    print(generate_validation_data("labels-testing.csv","../panos/", "sample"))
